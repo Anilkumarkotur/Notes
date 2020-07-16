5f55dac684c0bd76271ce46232c512c466d55890
Anilkumar kotur <Anilkumar.kotur@go-jek.com>
2020-07-16 20:03:25 +0530

    Adds sign up flow with resources to read

--- Sources/App/Models/User.swift
+++ Sources/App/Models/User.swift
@@ -17,17 +17,49 @@ final class User: SQLiteModel {
     var emailID: String
     var phoneNumber: String
     
+    var password: String
+    
     init(id: Int? = nil,
          name: String,
          profilePic: String,
          emailID: String,
-         phoneNumber: String) {
+         phoneNumber: String,
+         password: String) {
         
         self.id = id
         self.name = name
         self.profilePic = profilePic
         self.emailID = emailID
         self.phoneNumber = phoneNumber
+        self.password = password
+    }
+    
+    final public class PublicUser: Content {
+        var id: Int?
+        var name: String
+        var profilePic: String
+        var emailID: String
+        var phoneNumber: String
+        
+        init(name: String,
+             profilePic: String,
+             emailID: String,
+             phoneNumber: String) {
+            self.name = name
+            self.profilePic = profilePic
+            self.emailID = emailID
+            self.phoneNumber = phoneNumber
+        }
+        
+        public init(user: User) {
+            if let id = user.id  {
+                self.id = id
+            }            
+            self.name = user.name
+            self.profilePic = user.profilePic
+            self.emailID = user.emailID
+            self.phoneNumber = user.phoneNumber
+        }
     }
 }
 
@@ -38,12 +70,15 @@ extension User {
     }
 }
 
+extension User.PublicUser: SQLiteModel {
+    static let entity = User.entity
+}
+
+extension User.PublicUser: Parameter { }
+
 /// Allows `User` to be used as a dynamic migration.
 extension User: Migration { }
 /// Allows `User` to be encoded to and decoded from HTTP messages.
 extension User: Content { }
 /// Allows `User` to be used as a dynamic parameter in route definitions.
 extension User: Parameter { }
-
-
-
