d0b385ef4bd47225a96016b4c90d2873af51a095
Anilkumar kotur <Anilkumar.kotur@go-jek.com>
2020-11-13 17:05:01 +0530

    Updatet the config file to handle the db url based on the environemt and move the login route to public group

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -24,8 +24,21 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     services.register(dicConfig)
     
     var databases = DatabasesConfig()
-    let postgresDbConfig = PostgreSQLDatabaseConfig(hostname: "localhost", username: "anilkumarkotur", database: "noteappIntegration")
-    databases.add(database: PostgreSQLDatabase(config: postgresDbConfig), as: .psql)
+    
+    var postgreSQLDatabase: PostgreSQLDatabase
+    
+    if env.isRelease {
+        guard let dbURL = Environment.get("DATABASE_URL"), let prodConfig = PostgreSQLDatabaseConfig(url: dbURL) else {
+            print("Unable to fetch the database url")
+            return
+        }        
+        postgreSQLDatabase = PostgreSQLDatabase(config: prodConfig)
+    } else {
+        let postgresDbConfig = PostgreSQLDatabaseConfig(hostname: "localhost", username: "anilkumarkotur", database: "noteappIntegration")
+        postgreSQLDatabase = PostgreSQLDatabase(config: postgresDbConfig)
+    }
+    
+    databases.add(database: postgreSQLDatabase, as: .psql)
     services.register(databases)
 
     // Configure migrations
